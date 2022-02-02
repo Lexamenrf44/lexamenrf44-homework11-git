93db94561fc01bec2ff273e1f1fdfdbd096e9b4a
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-02-02 23:33:21 +0300

    Squashed 4 commits into 1

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,2 +1,35 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BasicTest {
+
+    @Test
+    @DisplayName("Тестирование 3 > 2")
+    public void test1() {
+        Assertions.assertTrue(3 > 2);
+
+    }
+
+    @Test
+    @DisplayName("Тестирование 4 > 2")
+    public void test2() {
+        Assertions.assertTrue(4 > 2);
+
+    }
+
+    @Test
+    @DisplayName("Тестирование 5 > 2")
+    public void test3() {
+        Assertions.assertTrue(5 > 2);
+
+    }
+
+    @Test
+    @DisplayName("Тестирование 6 > 3")
+    public void test4() {
+        Assertions.assertTrue(5 > 2);
+
+    }
+
 }
