b39b2d8df4d44d51d35e2644cae973164ee356b8
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-02-02 23:33:21 +0300

    Added AssertTrue in test branch

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class BasicTest {
+
+    @Test
+    @DisplayName("Тестирование 3 > 2")
+    public void test() {
+        Assertions.assertTrue(3 > 2);
+
+    }
+
 }
